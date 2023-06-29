355edfc46c954619b6b48c3d2c0da5e4a729e7ab
ElenaButnik <73306047+ElenaButnik@users.noreply.github.com>
2023-06-29 16:58:47 +0300

    add linters

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { Registration } from "./pages/Registration";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Registration />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Registration.js
@@ -0,0 +1,3 @@
+export const Registration = (params) => {
+  return <div>Hello</div>;
+};
