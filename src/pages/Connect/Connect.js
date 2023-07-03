4ea5a4ed5cb65279b1de8224461cc3761a9e7185
ElenaButnik <73306047+ElenaButnik@users.noreply.github.com>
2023-07-03 18:58:49 +0300

    add welcome page

---
+++ src/pages/Connect/Connect.js
@@ -0,0 +1,17 @@
+import { ReactComponent as Logo } from "../../images/icons/Logo.svg";
+import { ReactComponent as ButtonAdd } from "../../images/icons/ButtonAdd.svg";
+import s from "./Connect.module.scss";
+
+export const Connect = () => {
+  return (
+    <div className={s.container}>
+      <div className={s.logo}>
+        <Logo style={{ width: "112px", height: "140px" }} />
+      </div>
+      <h2 className={s.title}>CONNECT, COMMUNICATE, CONQUER LANGUAGES</h2>
+      <div className={s.button}>
+        <ButtonAdd />
+      </div>
+    </div>
+  );
+};
