56f3f69083c0f2aca2530096e5b9527149c8df8a
Andrew Litkovets <andriylao@gmail.com>
2023-07-03 23:50:39 +0300

    add Login component

--- src/pages/Connect/Connect.js
+++ src/pages/Connect/Connect.js
@@ -1,3 +1,4 @@
+import { NavLink } from "react-router-dom";
 import { ReactComponent as Logo } from "../../images/icons/Logo.svg";
 import { ReactComponent as ButtonAdd } from "../../images/icons/ButtonAdd.svg";
 import s from "./Connect.module.scss";
@@ -9,8 +10,13 @@ export const Connect = () => {
         <Logo style={{ width: "112px", height: "140px" }} />
       </div>
       <h2 className={s.title}>CONNECT, COMMUNICATE, CONQUER LANGUAGES</h2>
-      <div className={s.button}>
+      {/* <div className={s.button}>
         <ButtonAdd />
+      </div> */}
+      <div className={s.button}>
+        <NavLink to="/login">
+          <ButtonAdd />
+        </NavLink>
       </div>
     </div>
   );
